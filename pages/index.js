73ee48bc3c88681be6a12a5ba35af18c000ec58e
Will Warren <will.warren@groupbyinc.com>
2018-11-11 13:26:31 -0800

    removed cows

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import cowsay from 'cowsay-browser';
 import SignUp from '../components/SignUp';
 import Layout from '../components/Layout';
 const { parse } = require("url");
@@ -7,9 +6,6 @@ const Index = () => (
   <Layout>
     <img src="/static/img/dog-splash.jpg" height="200"/>
     <SignUp/>
-    <pre>
-      {cowsay.say({ text: 'mooooo' })}
-    </pre>
   </Layout>
 )
 
